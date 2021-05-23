1afd4f1ed2150d5322d44438d222d30a75aaf488
jennuwine <jennherrarte93@gmail.com>
2021-05-23 13:54:24 -0700

    changed route

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const homeCtrl = require('../controllers/index')
 
-router.get('/', function(req, res) {
-    res.redirect('/home')
-    })
 
-router.get('/home', homeCtrl.homePage)
+
+router.get('/', homeCtrl.homePage)
 router.get('/tweets/madeforlove', homeCtrl.madeforlove)
 router.get('/tweets/thehaunting', homeCtrl.blymanor)
 router.get('/tweets/lookingforalaska', homeCtrl.alaska)
