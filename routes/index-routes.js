e9b32aab834147926f63e64ff630fe07846e84a0
jennuwine <jennherrarte93@gmail.com>
2022-04-03 09:24:53 -0700

    update routes

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homeCtrl = require('../controllers/index')
 
-
-
 router.get('/', homeCtrl.homePage)
 router.get('/tweets/madeforlove', homeCtrl.madeforlove)
 router.get('/tweets/thehaunting', homeCtrl.blymanor)
@@ -14,8 +12,13 @@ router.get('/tweets/homebeforedark', homeCtrl.homeBeforeDark)
 router.get('/tweets/jackryan', homeCtrl.jackRyan)
 router.get('/tweets/paramounttelevisionstudios', homeCtrl.pTVS)
 router.get('/tweets/13reasonswhy', homeCtrl.reasonsWhy)
+router.get('/tweets/station11', homeCtrl.station11)
 
 module.exports = router; 
+
+
+
+
 
 
 
